c8d2279ee82d8cb3e9eb924f8cde01447b503bf5
Gabriel Bogado <gabrielgbogado92@gmail.com>
2023-10-15 17:02:50 -0300

    Trabajando en la visualización de datos de la API en Profile

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -1,7 +1,30 @@
 import React from "react";
 
-const Profile = () => {
-  return <div>Profile</div>;
+const Profile = (props) => {
+  return (
+    <div className="card  rounded mb-4 shadow">
+      <img src="" alt="" className="card-img " />
+      <div className="card-body">
+        <h5 className="card-title">Repo</h5>
+        <p className="card-text">Public</p>
+        <p className="card-text">Creado por Gabriel el 20 de Julio del 2013</p>
+      </div>
+      <div className="card-footer d-flex justify-content-between align-items-center">
+        <a
+          href="#"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="btn btn-primary"
+        >
+          Ver Repo
+        </a>
+        <div>
+          <span className="badge bg-secondary me-2">400 stars</span>
+          <span className="badge bg-secondary">200 Watchers</span>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Profile;
