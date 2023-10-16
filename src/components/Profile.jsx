be9a4822720884f2a0eb0f59df7f3fa84514c9be
Gabriel Bogado <gabrielgbogado92@gmail.com>
2023-10-16 03:16:38 -0300

    Refactorización de componente Profile

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -1,17 +1,46 @@
 import React from "react";
 
+// Función para formatear la fecha en un formato legible
+function formatDate(dateString) {
+  const options = { year: "numeric", month: "long", day: "numeric" };
+  const formattedDate = new Date(dateString).toLocaleDateString(
+    undefined,
+    options
+  );
+  return formattedDate;
+}
+
 const Profile = (props) => {
+  // Verificamos si la propiedad "owner" existe en las props
+  if (!props.owner) {
+    return null; // Si no existe, no mostramos nada y salimos de la función
+  }
+
   return (
-    <div className="card  rounded mb-4 shadow">
-      <img src="" alt="" className="card-img " />
+    <div className="card rounded mb-4 shadow">
+      <img
+        src={props.owner.avatar_url}
+        alt={props.owner.login}
+        className="card-img rounded-circle mt-3 shadow"
+        style={{ width: "50px", margin: "0 auto" }}
+      />
       <div className="card-body">
-        <h5 className="card-title">Repo</h5>
-        <p className="card-text">Public</p>
-        <p className="card-text">Creado por Gabriel el 20 de Julio del 2013</p>
+        <h5 className="card-title">{props.name}</h5>
+        <p className="card-text">
+          {" "}
+          {props.private ? (
+            <span className="badge bg-danger">Private</span>
+          ) : (
+            <span className="badge bg-success">Public</span>
+          )}
+        </p>
+        <p className="card-text">
+          Creado por {props.owner.login} el {formatDate(props.created_at)}
+        </p>
       </div>
       <div className="card-footer d-flex justify-content-between align-items-center">
         <a
-          href="#"
+          href={props.html_url}
           target="_blank"
           rel="noopener noreferrer"
           className="btn btn-primary"
@@ -19,8 +48,13 @@ const Profile = (props) => {
           Ver Repo
         </a>
         <div>
-          <span className="badge bg-secondary me-2">400 stars</span>
-          <span className="badge bg-secondary">200 Watchers</span>
+          <span className="badge bg-secondary me-2">
+            {" "}
+            {props.stargazers_count} stars
+          </span>
+          <span className="badge bg-secondary">
+            {props.watchers_count} Watchers
+          </span>
         </div>
       </div>
     </div>
